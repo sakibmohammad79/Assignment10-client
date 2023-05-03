d8503911d8ab0d130d4a63c12a7930a73c0654c1
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-05-03 16:36:58 +0600

    firebase config are hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlmA0L39WJSV8P7SNsRdHj5kwQTX6U5Z8",
-  authDomain: "assignment10-recipe-hunting.firebaseapp.com",
-  projectId: "assignment10-recipe-hunting",
-  storageBucket: "assignment10-recipe-hunting.appspot.com",
-  messagingSenderId: "619850981228",
-  appId: "1:619850981228:web:05bcf07e2d49191921dd49"
+  apiKey: import.meta.env.VITE_APIKEY ,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID ,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId: import.meta.env.VITE_APPID ,
 };
 
 // Initialize Firebase
